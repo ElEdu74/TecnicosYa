e1d3b60a53dcd4780d12f00d8961415f39ee7631
Eduardo Marchese <eledu2@gmail.com>
2022-08-27 19:46:21 -0300

    datos bd

--- config/config.js
+++ config/config.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'mysCobain74$',
-    database: 'tecnicos_ya'
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'mysCobain74$',
+    // database: 'tecnicos_ya'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bbab43d5154ee4',
+    password: 'c8fdb4b2',
+    database: 'heroku_c918f436f97afc1'
 });
 
 db.connect(function(err) {
