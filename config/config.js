4e57e6cd974dcd04dca66ff3f468a376da91a540
Eduardo Marchese <eledu2@gmail.com>
2022-08-27 20:50:20 -0300

    cambios conexion bd

--- config/config.js
+++ config/config.js
@@ -1,19 +1,29 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    // host: 'localhost',
-    // user: 'root',
-    // password: 'mysCobain74$',
-    // database: 'tecnicos_ya'
+// const db = mysql.createConnection({
+//     // host: 'localhost',
+//     // user: 'root',
+//     // password: 'mysCobain74$',
+//     // database: 'tecnicos_ya'
+//     host: 'us-cdbr-east-06.cleardb.net',
+//     user: 'bbab43d5154ee4',
+//     password: 'c8fdb4b2',
+//     database: 'heroku_c918f436f97afc1'
+// });
+
+const db = mysql.createPool({
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'bbab43d5154ee4',
     password: 'c8fdb4b2',
-    database: 'heroku_c918f436f97afc1'
+    database: 'heroku_c918f436f97afc1',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect(function(err) {
-    if (err) throw err;
-    console.log('DATABASE CONNECTED!');
-});
+// db.connect(function(err) {
+//     if (err) throw err;
+//     console.log('DATABASE CONNECTED!');
+// });
 
 module.exports = db;
